a1946abdd63195782bdb9ef97256d123d64c343f
Jonas <68380809+jonalbr@users.noreply.github.com>
2025-09-13 20:21:05 +0200

    chore(release): v1.4.1

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ if sys.platform == "win32":
 
 setup(
     name="Heat Sheet PDF Highlighter",
-    version="1.4.0",
+    version="1.4.1",
     description="Heat Sheet PDF Highlighter",
     author="Jonas Albrecht",
     maintainer="Jonas Albrecht",
--- src/constants.py
+++ src/constants.py
@@ -4,7 +4,7 @@ Application constants
 
 # Application metadata
 APP_NAME = "Heat Sheet PDF Highlighter"
-VERSION_STR = "1.4.0"
+VERSION_STR = "1.4.1"
 
 # Supported languages
 LANGUAGE_OPTIONS = ["en", "de"]  # Add more languages as needed
