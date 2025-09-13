f0ec4c57bcc12407f53caef67451765d4001a567
Jonas <68380809+jonalbr@users.noreply.github.com>
2025-09-13 21:51:51 +0200

    chore(release): v1.4.3

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ if sys.platform == "win32":
 
 setup(
     name="Heat Sheet PDF Highlighter",
-    version="1.4.2",
+    version="1.4.3",
     description="Heat Sheet PDF Highlighter",
     author="Jonas Albrecht",
     maintainer="Jonas Albrecht",
